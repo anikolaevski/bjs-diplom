eb9ce41300d45c9d3cb8a72711dc70b7f6bb3a76
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-04-24 16:44:15 +0300

    v0.2

--- static/main.js
+++ static/main.js
@@ -1,11 +1,12 @@
 "use strict";
 class Profile {
+
   constructor (data) {
     this.username = data.username;
     this.name = data.name;
     this.password = data.password;
-    this.authorized = false;
-    this.balance = 0;
+    this.wallet = {RUB: 0, USD: 0, EUR: 0, NETCOIN: 0};
+    this.createStatus = 0;
     ApiConnector.createUser( 
       {
         username: this.username,
@@ -14,9 +15,13 @@ class Profile {
       }, 
       (err, data) => {
         console.log(`creating user ${this.username}... error ${err}, ${data}`);
+        if(!err) {
+          this.createStatus = 1;
+        }
        }
      );    
   }
+  
   login() {
     ApiConnector.performLogin(
       {
@@ -28,21 +33,61 @@ class Profile {
        }
      );
    }
-}
+   
+   addMoney( { currency, amount }, callback) {
+        return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+            console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+            callback(err, data);
+        });
+    }
 
+}
 const Ivan = new Profile({
                   username: 'ivan',
                   name: { firstName: 'Ivan', lastName: 'Chernyshev' },
                   password: 'ivanspass'
               });
 
-function catchError ( err, data ) {
-  console.log(err, data);
-}
+//function catchError ( err, data ) {
+//  console.log(err, data);
+//}
 
-const err1 = () => catchError(1,'asd');
+//const err1 = () => catchError(1,'asd');
 
-console.log(Ivan);
-console.log(err1());
+//console.log('Created profile:',Ivan);
+//console.log(err1());
+
+
+const Petr = new Profile({
+                  username: 'petr',
+                  name: { firstName: 'Petr', lastName: 'Petroff' },
+                  password: 'petrpass'
+              });
+
+/*
+ApiConnector.createUser( 
+      {
+                  username: 'ivan3',
+                  name: { firstName: 'Ivan', lastName: 'Chernyshev' },
+                  password: 'ivanspass'
+      }, 
+      (err, data) => {
+        console.log(`creating user 'ivan3'... error ${err}, ${data}`);
+        return data;
+       }
+     );
+*/
 
 //setTimeout(Ivan.login,5000);
+
+/*
+Ivan.login();
+     
+Ivan.addMoney(
+  {
+    currency: 'USD',
+    amount: 1000
+  }, 
+  ( err, data ) => {console.log(err, data)}
+);
+*/
